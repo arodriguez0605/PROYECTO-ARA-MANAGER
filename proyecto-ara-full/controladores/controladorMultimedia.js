547d26c8a85584e4d0bef8c3f056b70b491df459
Alejandra Rodríguez <a_rodriguez0605@hotmail.com>
2019-10-02 18:32:00 -0600

    Banco multimedia
    Pruebas en el controlador para el banco multimedia

---
+++ proyecto-ara-full/controladores/controladorMultimedia.js
@@ -0,0 +1,12 @@
+function cargarMultimedia() {
+    var nombreArchivo = document.getElementById("nombreArchivo").value;
+    var descripcionArchivo = document.getElementById("descripcionArchivo").value;
+    var cargarArchivo = document.getElementById("cargarArchivo").value;
+
+   alert("hola" +nombreArchivo);
+    console.log("Se esta imprimiendo algo xD" +nombreArchivo);
+    console.log("Se esta imprimiendo algo xD" +descripcionArchivo);
+    console.log("Se esta imprimiendo algo xD" +cargarArchivo);
+    
+ 
+};
--- proyecto-ara-full/public/assets/js/controlador.js
+++ proyecto-ara-full/public/assets/js/controlador.js
@@ -137,3 +137,16 @@ $("#btn-logout").click(function(){
     }
   });
 });
+
+
+function cargarMultimedia() {
+  var nombreArchivo = document.getElementById("nombreArchivo").value;
+  var descripcionArchivo = document.getElementById("descripcionArchivo").value;
+  var cargarArchivo = document.getElementById("cargarArchivo").value;
+
+
+  console.log("Se esta imprimiendo algo xD" +nombreArchivo);
+  console.log("Se esta imprimiendo algo xD" +descripcionArchivo);
+  console.log("Se esta imprimiendo algo xD" +cargarArchivo);
+  
+};

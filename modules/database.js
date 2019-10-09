4f6dc15928a5139a60871255eb9ac26317b155e1
arodriguez0605 <a_rodriguez0605@hotmail.com>
2019-10-09 15:29:46 -0600

    probando mlab

--- modules/database.js
+++ modules/database.js
@@ -3,16 +3,30 @@ var mongoose = require('mongoose');
 var servidor = 'arodriguez:arodriguez2@ds333238.mlab.com:33238';
 var db = 'heroku_mqpv9nkt';
 
-class Database{
-    constructor(){
-        // Promesas
-        mongoose.connect(`mongodb://${servidor}/${db}`)
-        .then(()=>{
-            console.log('Se conecto a mongo');
-        }).catch((error)=>{
-            console.log(error);
-        });
-    }
+class Database {
+  constructor(){
+      // Promesas
+      mongoose.connect(`mongodb://heroku_mqpv9nkt:@Queteimporta1@ds333238.mlab.com:33238/heroku_mqpv9nkt`)
+      .then(()=>{
+          console.log('Se conecto a mongo');
+      }).catch((error)=>{
+          console.log(error);
+      });
+  }
 }
+
+
+
+// class Database {
+//     constructor(){
+//         // Promesas
+//         mongoose.connect(`mongodb://${servidor}/${db}`)
+//         .then(()=>{
+//             console.log('Se conecto a mongo');
+//         }).catch((error)=>{
+//             console.log(error);
+//         });
+//     }
+// }
 
 module.exports = new Database();

a7398df29cbb06432f4c57fe7d8bf7765b194cde
Alejandra Rodriguez <a_rodriguez0605@hotmail.com>
2019-10-08 22:36:12 -0500

    cargar miniaturas de multimedia desde la base de datos

--- proyecto-ara-full/server.js
+++ proyecto-ara-full/server.js
@@ -30,9 +30,44 @@ let storageEntrada = multer.diskStorage({
 
 const uploadCategoria = multer({ storage: storageCategoria });
 const uploadEntrada = multer({ storage: storageEntrada });
-const uploadImage = multer({ dest: './public/data/archivosSubidos/bancoImagenes/' });
-const uploadVideo = multer({ dest: './public/data/archivosSubidos/bancoVideos/' });
-const uploadArchivo = multer({ dest: './public/data/archivosSubidos/bancoArchivos/' });
+const uploadImage = multer({ storage: multer.diskStorage({
+  destination: function (req, file, callback) { callback(null, './public/data/archivosSubidos/bancoImagenes/');},
+  filename: function (req, file, callback) {
+    callback(null,
+      `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`);}
+    })
+});
+const uploadVideo = multer({ storage: multer.diskStorage({
+  destination: function (req, file, callback) { callback(null, './public/data/archivosSubidos/bancoVideos/');},
+  filename: function (req, file, callback) {
+    callback(null,
+      `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`);}
+    })
+});
+const uploadArchivo = multer({ storage: multer.diskStorage({
+  destination: function (req, file, callback) { callback(null, './public/data/archivosSubidos/bancoArchivos/');},
+  filename: function (req, file, callback) {
+    callback(null,
+      `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`);}
+    })
+});
+
+const separarMultimedia = (multimedias) => {
+  let bancoImagenes = [];
+  let bancoVideos = [];
+  let bancoArchivos = [];
+
+  multimedias.map(multimedia => {
+    if (multimedia.extensionArchivo.split('/')[0] === 'image') {
+      bancoImagenes.push(multimedia);
+    } else if (multimedia.extensionArchivo.split('/')[0] === 'video') {
+      bancoVideos.push(multimedia);
+    } else {
+      bancoArchivos.push(multimedia);
+    }
+  })
+  return { bancoImagenes, bancoVideos, bancoArchivos }
+}
 
 // Puerto 
 const port = process.env.PORT || 3000;
@@ -77,8 +112,10 @@ app.get('/navbar', verificarAuth, (req, res) => {
   res.render('/parciales/navbar');
 });
 
-app.get('/multimedia', verificarAuth, (req, res) => {
-  res.render('multimedia');
+app.get('/multimedia', verificarAuth, async (req, res) => {
+  const multimedias = await Multimedia.obtenerMultimedias();
+  const multimedia = separarMultimedia(multimedias);
+  res.render('multimedia', { imagenes: multimedia.bancoImagenes, videos: multimedia.bancoVideos, archivos: multimedia.bancoArchivos });
 });
 
 app.get('/perfil', verificarAuth, (req, res) => {
@@ -93,22 +130,22 @@ app.get('/categorias', verificarAuth, (req, res) => {
   var categorias = Categoria.obtenerCategorias();
   // console.log(categorias);
 
-  categorias.then(function(categorias){
+  categorias.then(function (categorias) {
     // console.log(categorias);
     console.log(req.session.user);
-    res.render('categorias', {categoria: categorias});
- })
-  
+    res.render('categorias', { categoria: categorias });
+  })
+
 });
 
 app.get('/entradas', verificarAuth, (req, res) => {
   var entradas = Entrada.obtenerEntradas();
   // console.log(categorias);
 
-  entradas.then(function(entradas){
+  entradas.then(function (entradas) {
     console.log(entradas);
-  //console.log(req.session.user);
-    res.render('entradas', {entrada: entradas});
+    //console.log(req.session.user);
+    res.render('entradas', { entrada: entradas });
   })
 });
 
@@ -117,41 +154,62 @@ app.get('/editor', verificarAuth, (req, res) => {
   var categorias = Categoria.obtenerCategorias();
   // console.log(categorias);
 
-  categorias.then(function(categorias){
+  categorias.then(function (categorias) {
     // console.log(categorias);
     console.log(req.session.user);
-    res.render('editor', {categoria: categorias});
- });
+    res.render('editor', { categoria: categorias });
+  });
 });
 
 app.get('/editor2', verificarAuth, (req, res) => {
   res.render('editor2');
 });
 
-app.post('/subirImagen', uploadImage.single('cargarArchivo'), async (req, res) => {
+app.post('/subirImagen', uploadImage.single('cargarImagen'), async (req, res) => {
   try {
     if (
       req.body.nombreArchivo !== '' &&
       req.body.descripcionArchivo !== '' &&
       req.file !== undefined
     ) {
-      let extensionArchivo = req.file.mimetype.split('/');
       let imagenData = {
         nombre: req.body.nombreArchivo,
         descripcion: req.body.descripcionArchivo,
-        urlImagen: `/public/data/archivosSubidos/bancoImagenes/${req.file.filename}.${extensionArchivo[1]}`,
-        extensionArchivo: `.${extensionArchivo[1]}`,
+        urlImagen: `/data/archivosSubidos/bancoImagenes/${req.file.filename}`,
+        extensionArchivo: req.file.mimetype,
       }
 
-      await Multimedia.guardarImagen(imagenData, res).then(response => {
-        res.render('multimedia', { code: 200, message: 'La imagen ha sido guardada!' });
+      const multimedias = await Multimedia.obtenerMultimedias();
+      const multimedia = separarMultimedia(multimedias);
+
+      await Multimedia.guardarImagen(imagenData, res).then(async response => {
+
+        res.render('multimedia', {
+          imagenes: multimedia.bancoImagenes,
+          videos: multimedia.bancoVideos,
+          archivos: multimedia.bancoArchivos,
+          code: 200,
+          message: 'La imagen ha sido guardada!'
+        });
       })
 
     } else {
-      res.render('multimedia', { code: 400, message: 'Datos incompletos.' });
+      res.render('multimedia', {
+        imagenes: multimedia.bancoImagenes,
+        videos: multimedia.bancoVideos,
+        archivos: multimedia.bancoArchivos,
+        code: 400,
+        message: 'Datos incompletos.'
+      });
     }
   } catch (error) {
-    res.render('multimedia', { code: 500, message: error });
+    res.render('multimedia', {
+      imagenes: multimedia.bancoImagenes,
+      videos: multimedia.bancoVideos,
+      archivos: multimedia.bancoArchivos,
+      code: 500,
+      message: error
+    });
   }
 })
 
@@ -162,23 +220,43 @@ app.post('/subirVideo', uploadVideo.single('cargarVideo'), async (req, res) => {
       req.body.descripcionVideo !== '' &&
       req.file !== undefined
     ) {
-      let extensionVideo = req.file.mimetype.split('/');
       let imagenData = {
         nombre: req.body.nombreVideo,
         descripcion: req.body.descripcionVideo,
-        urlVideo: `/public/data/archivosSubidos/bancoVideos/${req.file.filename}.${extensionVideo[1]}`,
-        extensionVideo: `.${extensionVideo[1]}`,
+        urlVideo: `/data/archivosSubidos/bancoVideos/${req.file.filename}`,
+        extensionVideo: req.file.mimetype,
       }
 
-      await Multimedia.guardarVideo(imagenData, res).then(response => {
-        res.render('multimedia', { code: 200, message: 'El video ha sido guardado!' });
+      const multimedias = await Multimedia.obtenerMultimedias();
+      const multimedia = separarMultimedia(multimedias);
+
+      await Multimedia.guardarVideo(imagenData, res).then(async response => {
+        res.render('multimedia', {
+          imagenes: multimedia.bancoImagenes,
+          videos: multimedia.bancoVideos,
+          archivos: multimedia.bancoArchivos,
+          code: 200,
+          message: 'El video ha sido guardado!',
+        });
       })
 
     } else {
-      res.render('multimedia', { code: 400, message: 'Datos incompletos.' });
+      res.render('multimedia', {
+        imagenes: multimedia.bancoImagenes,
+        videos: multimedia.bancoVideos,
+        archivos: multimedia.bancoArchivos,
+        code: 400,
+        message: 'Datos incompletos.',
+      });
     }
   } catch (error) {
-    res.render('multimedia', { code: 500, message: error });
+    res.render('multimedia', {
+      imagenes: multimedia.bancoImagenes,
+      videos: multimedia.bancoVideos,
+      archivos: multimedia.bancoArchivos,
+      code: 500,
+      message: error,
+    });
   }
 })
 
@@ -189,23 +267,44 @@ app.post('/subirArchivo', uploadArchivo.single('cargarArchivo'), async (req, res
       req.body.descripcionArchivo !== '' &&
       req.file !== undefined
     ) {
-      let extensionArchivo = req.file.mimetype.split('/');
       let archivoData = {
         nombre: req.body.nombreArchivo,
         descripcion: req.body.descripcionArchivo,
-        urlArchivo: `/public/data/archivosSubidos/bancoArchivos/${req.file.filename}.${extensionArchivo[1]}`,
-        extensionArchivo: `.${extensionArchivo[1]}`,
+        urlArchivo: `/data/archivosSubidos/bancoArchivos/${req.file.filename}`,
+        extensionArchivo: req.file.mimetype,
       }
 
-      await Multimedia.guardarArchivo(archivoData, res).then(response => {
-        res.render('multimedia', { code: 200, message: 'El archivo ha sido guardado!' });
+      const multimedias = await Multimedia.obtenerMultimedias();
+      const multimedia = separarMultimedia(multimedias);
+
+      await Multimedia.guardarArchivo(archivoData, res).then(async response => {
+
+        res.render('multimedia', {
+          imagenes: multimedia.bancoImagenes,
+          videos: multimedia.bancoVideos,
+          archivos: multimedia.bancoArchivos,
+          code: 200,
+          message: 'El archivo ha sido guardado!',
+        });
       })
 
     } else {
-      res.render('multimedia', { code: 400, message: 'Datos incompletos.' });
+      res.render('multimedia', {
+        imagenes: multimedia.bancoImagenes,
+        videos: multimedia.bancoVideos,
+        archivos: multimedia.bancoArchivos,
+        code: 400,
+        message: 'Datos incompletos.',
+      });
     }
   } catch (error) {
-    res.render('multimedia', { code: 500, message: error });
+    res.render('multimedia', {
+      imagenes: multimedia.bancoImagenes,
+      videos: multimedia.bancoVideos,
+      archivos: multimedia.bancoArchivos,
+      code: 500,
+      message: error
+    });
   }
 })
 
@@ -257,7 +356,7 @@ app.post('/crearEntrada', uploadEntrada.single('cargarImagen'), async (req, res)
         estado = 1
       } else {
         estado = req.body.estado
-      }      
+      }
       console.log(req.body);
       console.log('req.file----->>>>>>', req.file);
       //let extensionArchivo = req.file.mimetype.split('/');
